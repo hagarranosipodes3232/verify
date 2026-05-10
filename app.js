af663131b754d9b3a35f1a5ef90c049dc0421437
Juancito <juancito@gmail.com>
2026-05-10 05:11:49 -0300

    mongodb

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const web = express();
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("✅ MongoDB conectado"))
+.catch(err => console.log(err));
 
 // PAGINA PRINCIPAL
 
